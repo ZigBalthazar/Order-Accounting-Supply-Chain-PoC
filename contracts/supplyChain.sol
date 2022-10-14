7f7c5a48b6e2c86969d03a63eb9aa34f3a1ab0e0
pouya <pouya@radhoosh.ir>
2022-10-14 18:22:39 +0330

    createOrder function

--- contracts/supplyChain.sol
+++ contracts/supplyChain.sol
@@ -4,15 +4,21 @@ pragma solidity 0.8.17;
 contract supplyChainManagement{
 
  
- enum status { created,delivering,deliverd,accepted,declined}
- struct order{
-    string title;
-    string descriptoin;
-    address supplier;
-    address deliveryCompany;
-    address customer;
-    // status _status;
- }   
+    enum status { created,delivering,deliverd,accepted,declined}
+    struct order{
+        string title;
+        string descriptoin;
+        address supplier;
+        address deliveryCompany;
+        address customer;
+        status _status;
+    }   
+    order[] ordrers;
 
- 
+    function createOrder(string memory _title, string memory _description, address _deliveryCompany, address _customer) public returns(uint){
+        order memory _order = order(_title,_description,msg.sender,_deliveryCompany, _customer, status.created);
+        uint index = ordrers.length - 1;
+        ordrers[index] = _order;
+        return index;
+    }
 }
