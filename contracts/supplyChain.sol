69f9f36a71555b5c81c878f50c8194251538256a
pouya <pouya@radhoosh.ir>
2022-10-15 09:15:52 +0330

    acceptOrder

--- contracts/supplyChain.sol
+++ contracts/supplyChain.sol
@@ -20,6 +20,28 @@ contract supplyChainManagement{
         require(ordrers[_index].deliveryCompany == msg.sender);
         _;
     }
+
+    
+    modifier onlyCustomer(uint _index){
+        require(ordrers[_index].customer == msg.sender);
+        _;
+    }
+
+    modifier orderCreated(uint _index){
+        require(ordrers[_index]._status == status.created);
+        _;
+    }
+
+    modifier orderDelivering(uint _index) {
+        require(ordrers[_index]._status == status.delivering);
+        _;
+    }
+
+    modifier orderDelivered(uint _index) {
+        require(ordrers[_index]._status == status.deliverd);
+        _;
+    }
+
     function createOrder(string memory _title, string memory _description, address _deliveryCompany, address _customer) public returns(uint){
         order memory _order = order(_title,_description,msg.sender,_deliveryCompany, _customer, status.created);
         uint index = ordrers.length - 1;
@@ -28,14 +50,18 @@ contract supplyChainManagement{
     }
 
 
-    function startDeliveringOrder(uint _index)public onlyOrderDeliveringCompany(_index) {
+    function startDeliveringOrder(uint _index)public onlyOrderDeliveringCompany(_index) orderCreated(_index) {
         order storage _order = ordrers[_index];
         _order._status = status.delivering;
     }
 
-    function stopDeliveringOrder(uint _index) public onlyOrderDeliveringCompany(_index){
+    function stopDeliveringOrder(uint _index) public onlyOrderDeliveringCompany(_index) orderDelivering(_index){
         order storage _order = ordrers[_index];
         _order._status = status.deliverd;
     }
+
+    function acceptOrder(uint _index) public onlyCustomer(_index) orderDelivered(_index){
+        ordrers[_index]._status = status.accepted;
+    } 
 
 }
